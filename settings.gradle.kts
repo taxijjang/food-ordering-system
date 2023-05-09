3ddc0fc74821358f2780c948c44ef02f09e424b0
김택윤 <gw9122@naver.com>
2023-05-09 14:02:33 +0900

    project init

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+
+rootProject.name = "food-ordering-system"
+
+include("order-application")
+include("order-container")
+include("order-dataaccess")
+include("order-domain")
+include("order-messaging")
+include("order-application")
+include("order-domain:order-application-service")
+include("order-domain:order-domain-core")
